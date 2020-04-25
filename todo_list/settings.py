a1463f8e043032f897c2c4a98a27688ad0dc19f0
Jedi <jedi@MacBook-Air-Jedi.local>
2020-04-26 02:28:28 +0300

    adapt frontend for django

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo_list_backend',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static', 'js'),
+    os.path.join(BASE_DIR, 'static', 'css'),
+)
---
+++ todo_list_backend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TodoListBackendConfig(AppConfig):
+    name = 'todo_list_backend'
